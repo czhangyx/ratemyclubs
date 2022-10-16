a1ee06721a2c6e4ac1a48271711a7b3d9f3b57ca
Evelyn <evelyn0307@berkeley.edu>
2022-10-15 20:19:12 -0700

    main new

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,17 @@
+const schoolList = {school1: berkeley}
+const berkeley = {club1: ASUC, club2: BerkeleyArtStudio, club3: CalGreeks}
+const location = {href: 'file:///Users/xinyizheng/Desktop/rmc/ratemyclubs/rmc.html'}
+const ASUC = {info: location.href}
+const BerkeleyArtStudio = {info: "Find art classes for the campus community and the public, from digital design to drawing to ceramics."}
+const CalGreeks = {info: "Connect with alumni and stay connected to Berkeley after graduation."}
+
 function submission_confirmation(e) {
     e.preventDefault();
 
     var school =  document.getElementById("school_name").value
     var club =  document.getElementById("club_name").value
 
-    console.log("name" + school)
-    console.log("name" + club)
+    console.log(school)
+    console.log(club + club.info)
 
 }
