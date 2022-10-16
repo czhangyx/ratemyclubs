d7ae5d991a6488eebf429c44fa9b4f624e87418d
Coco Zhang <czhangyx@berkeley.edu>
2022-10-16 12:18:22 -0700

    12:18

--- scripts/clubpage.js
+++ scripts/clubpage.js
@@ -1,15 +1,18 @@
 function post_comment(e, comments) {
     e.preventDefault();
 
-    var comment =  document.getElementById("comment-input").value;
-    comments.push(comment);
+    var comment = document.getElementById("comment-input").value;
+    var ip = $.getJSON('https://api.ipify.org?format=jsonp&callback=?', function(data) {
+        console.log(JSON.stringify(data, null, 2));
+      });
+    console.log(ip);
+    var ipComment = {ip: comment};
+    comments.push(ipComment);
     location.reload();
 }
 
 function delete_comment(e, comments) {
     e.preventDefault();
-
-    
 
     comments.removeLast();
     location.reload();
--- scripts/search.js
+++ scripts/search.js
@@ -1,9 +1,9 @@
-function click_club(e) {
+function click_club(e, clubname) {
     e.preventDefault();
 
-    var club = document.getElementById("").value;
+    console.log(clubname);
     var infoComment = localStorage.getItem("club-list");
-    localStorage.setItem("club-name", club);
+    localStorage.setItem("club-name", clubname);
     localStorage.setItem("info-comments", infoComment);
 
     window.location.href = "club-page.html";
